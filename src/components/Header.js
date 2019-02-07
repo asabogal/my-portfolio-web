76fbe9b6d209fdb1652d26c382b23ec573470223
asabogal <asabogal.git@gmail.com>
2019-02-06 21:36:17 -0500

    first header; need better img

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <header className="App-header">
+        <div className="caption-text">
+          alejandro sabogal<br></br>
+          full-stack web-developer
+        </div>
+    </header>
+  );
+};
+
+export default Header;
