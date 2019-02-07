bc4bdf37619d7b9f82b35bec43f313cf70df929f
asabogal <asabogal.git@gmail.com>
2019-02-06 22:30:38 -0500

    intro placeholder; add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
+import Intro from './components/Intro'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
+        <Intro/>
       </div>
     );
   }
